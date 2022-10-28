0c9b657ec2db57bc62425cf775c9bffe8b1958cd
ray-chunkit-chung <ray.chunkit.chung@gmail.com>
2022-10-28 01:22:59 +0000

    hello world

---
+++ logistics/contracts/HelloWorld.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.17;
+
+contract HelloWorld {
+    string private helloMessage = "Hello world";
+
+    function getHelloMessage() public view returns (string memory) {
+        return helloMessage;
+    }
+}
