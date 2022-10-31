e9067fa970de83f7224d24c996b77f47653744c6
ray-chunkit-chung <ray.chunkit.chung@gmail.com>
2022-10-31 05:01:36 +0000

    try supply chain

---
+++ supplyChainTry/contracts/basicMath.sol
@@ -0,0 +1,13 @@
+pragma solidity 0.8.17;
+
+contract basicMath {
+    uint256 private constant MAX_UINT256 = 2**256 - 1;
+
+    function add(uint256 _numberA, uint256 _numberB)
+        public
+        pure
+        returns (uint256)
+    {
+        return _numberA + _numberB;
+    }
+}
